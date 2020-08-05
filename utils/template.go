4371b7b189ca231da9e5399001f0ef3bf859a3ce
Ariel Diaz Perez <ardipe92@gmail.com>
2020-08-05 03:26:38 -0400

    DONE: load files contents

--- utils/template.go
+++ utils/template.go
@@ -36,6 +36,10 @@ func New(filename, root, tmplName string) (Template, error) {
 		} else if strings.HasSuffix(line, "/") {
 			line = line[:len(line)-1]
 			dirs = append(dirs, line)
+		} else if i := strings.LastIndex(line, "/"); i != -1 {
+			dir := line[:i+1]
+			dirs = append(dirs, dir)
+			files = append(files, line)
 		} else {
 			files = append(files, line)
 		}
@@ -52,14 +56,19 @@ func (t Template) Build(norepo bool) error {
 		if err != nil {
 			return err
 		}
+		fmt.Printf("> Created dir: %s\n", dir)
 	}
 	// Make files
 	for _, file := range t.Files {
+		_, filename := splitFilename(file)
+		content := loadContent(t.Name, filename)
+		content = replaceTokens(content, t.Root)
 		file = filepath.Join(t.Root, file)
-		err := ioutil.WriteFile(file, []byte(""), 0644)
+		err := ioutil.WriteFile(file, content, 0644)
 		if err != nil {
 			return err
 		}
+		fmt.Printf("> Created file: %s\n", file)
 	}
 	if norepo {
 		return nil
@@ -92,4 +101,29 @@ func fixLine(line string) string {
 	}
 	line = strings.TrimRight(line, " ")
 	return line
+}
+
+// loadContent load all the content of an specific file
+func loadContent(tmpl, filename string) []byte {
+	contentPath := os.Getenv("GOPS_CONTENT")
+	content, err := ioutil.ReadFile(contentPath + tmpl + "/" + filename)
+	if err == nil {
+		return content
+	}
+	content, err = ioutil.ReadFile(contentPath + "commun/" + filename)
+	if err == nil {
+		return content
+	}
+	return []byte("")
+}
+
+func splitFilename(file string) (string, string) {
+	if i := strings.LastIndex(file, "/"); i != -1 {
+		return file[:i+1], file[i+1:] // dir, filename
+	}
+	return "", file
+}
+
+func replaceTokens(content []byte, projectName string) []byte {
+	return []byte(strings.ReplaceAll(string(content), "{# projectName #}", projectName))
 }
